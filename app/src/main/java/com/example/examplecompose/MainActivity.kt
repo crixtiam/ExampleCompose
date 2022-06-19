e57fdf49698a0ca6a7178d3ba90095877885e902
cristiam <cristiam9211@gmail.com>
2022-06-19 12:28:30 -0500

    Tutorial_compose_kotlin

---
+++ app/src/main/java/com/example/examplecompose/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.examplecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.examplecompose.ui.theme.ExampleComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExampleComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true,
+    name = "Android Theme",
+    widthDp = 400,
+    heightDp = 200
+    )
+
+@Composable
+fun DefaultPreview() {
+    ExampleComposeTheme {
+        Box(
+            modifier= Modifier.fillMaxSize().background(Color.Yellow)
+        ){
+            Greeting("Android")
+            Greeting("Cristiam")
+
+        }
+        Greeting("Android")
+    }
+}
