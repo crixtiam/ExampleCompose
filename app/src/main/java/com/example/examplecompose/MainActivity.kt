626bf185172678b4252810363560b692a8807a68
cristiam <cristiam9211@gmail.com>
2022-06-19 12:43:35 -0500

    Tutorial_compose_kotlin

--- app/src/main/java/com/example/examplecompose/MainActivity.kt
+++ app/src/main/java/com/example/examplecompose/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : ComponentActivity() {
             ExampleComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
